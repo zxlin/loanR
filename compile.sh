b0dc4841802b9bce103b16af173e6e49e4a63c3b
Zhi Xiang Lin <zhixiang94@gmail.com>
2015-04-11 02:51:21 -0400

    updated index

--- compile.sh
+++ compile.sh
@@ -3,3 +3,4 @@
 compile="$(which clientjade) -c"
 
 $compile ./templates/index.jade > ./packages/system/public/assets/jade/index.js
+$compile ./templates/lender.jade > ./packages/system/public/assets/jade/lender.js
