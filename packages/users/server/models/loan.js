8db963fe385c5b48fa1241e0042c6a247fb5d422
Andrew Liu <liu.andrew.x@gmail.com>
2015-04-11 04:36:54 -0400

    updated schema types for loan and post

--- packages/users/server/models/loan.js
+++ packages/users/server/models/loan.js
@@ -3,10 +3,30 @@
 var UserSchema = new Schema({
   lender: {},
   borrower: {},
-  monthly_fee: int,
-  amount_left_total: int,
-  monthly_fee_left_to_pay_this_month: int,
-  pay_day: int,
+  monthly_fee: {
+    type: Number,
+    required: true
+  },
+  amount_left_total: {
+    type: Number,
+    required: true
+  },
+  monthly_fee_left_to_pay_this_month: {
+    type: Number,
+    required: true
+  },
+  pay_day: {
+    type: Number,
+    required: true
+  },
+  missed_last_payment: {
+    type: boolean,
+    required: true 
+  },
+  estimated_time_left: {
+    type: Number,
+    required: true
+  }
   // need to complete
 });
 
--- packages/users/server/models/post.js
+++ packages/users/server/models/post.js
@@ -1,8 +1,23 @@
 var UserSchema = new Schema({
   poster: {},
-  amount: int,
-  interest: int,
-  monthly_bill: int,
-  estimated_completion: Date,
-  desired_rating: int
+  amount: {
+    type: Number,
+    required: true
+  },
+  interest: {
+    type: Number,
+    required: true
+  },
+  monthly_bill: {
+    type: Number,
+    required: true
+  },
+  estimated_completion: {
+    type: Date,
+    required: true
+  },
+  desired_rating: {
+    type: Date,
+    required: true
+  }
 });
