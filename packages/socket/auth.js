4cba9243dc73001a92c5cf5eae19ee9d8e24f83a
fpayer <fpayer@tacnetsol.com>
2015-04-11 15:03:10 -0400

    Added user registration and login

--- packages/socket/auth.js
+++ packages/socket/auth.js
@@ -15,7 +15,7 @@ ret.createPost = function(data) {
     amount : data.amount,
     interest : data.interest,
     monthly_bill : data.monthly_bill,
-    estimated_completion : 0,
+    estimated_completion : 0, //TODO Save
     desired_rating : data.desired_rating,
     lender : user.user_role
   });
@@ -66,7 +66,7 @@ ret.takeLoan = function(data) {
         monthly_fee_left_to_pay_this_month : post.monthly_fee,
         pay_day : (new Date()).getDay(),
         missed_last_payment : false,
-        estimated_time_left: 0
+        estimated_time_left: 0 //TODO use func
       });
       loan.save(function(err) {
         d(err, loan);
--- packages/system/public/assets/js/index.js
+++ packages/system/public/assets/js/index.js
@@ -19,4 +19,33 @@ $(document).ready(function(){
     $('#main-borrower-btn').addClass('blue')
     $('#main-lender-btn').removeClass('red');
   });
+  $('#registration-btn').on('click', function() {
+    var name = $('#registration-first-name').val();
+    var account = $('#registration-account').val();
+    var email = $('#registration-email').val();
+    var pass1 = $('#registration-password').val();
+    var pass2 = $('#registration-confirm-password').val();
+
+    if (pass1 !== pass2) {
+      alert('Passwords do not match');
+    } else {
+      socket.emit('createAccount', {
+        name : name,
+        accnumber : account,
+        email : email,
+        user_role : is_lender ? 'Lender' : 'Borrower',
+        password : pass1
+      });
+    }
+  });
+  $('#login-btn').on('click', function() {
+    $.post('/login', {
+      email : $('#login-email').val(),
+      password : $('#login-password').val()
+    }).done(function(data) {
+      window.location = data.redirect;
+    }).fail(function(data) {
+      alert('Username and passwords do not match.');
+    });
+  });
 });
