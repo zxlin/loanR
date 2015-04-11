d0415a71022148b34783a64ae4ca041ef7bb1141
Zhi Xiang Lin <zhixiang94@gmail.com>
2015-04-11 07:15:10 -0400

    finished most of UI

--- packages/system/public/assets/js/index.js
+++ packages/system/public/assets/js/index.js
@@ -1,9 +1,22 @@
+var is_lender = false;
+var is_borrower = false;
+
 $(document).ready(function(){
   $('#login-register-btn').on('click', function(){
-    console.log('clicking');
     $('#registration-modal')
       .modal('show')
     ;
   });
-
+  $('#main-lender-btn').on('click', function(){
+    is_lender = true;
+    is_borrower = false;
+    $('#main-lender-btn').addClass('red');
+    $('#main-borrower-btn').removeClass('blue');
+  });
+  $('#main-borrower-btn').on('click', function(){
+    is_borrower = true;
+    is_lender = false;
+    $('#main-borrower-btn').addClass('blue')
+    $('#main-lender-btn').removeClass('red');
+  });
 });
