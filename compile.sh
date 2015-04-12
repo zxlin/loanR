413d04921f32e9bae06056cf7d2a01a7229bd026
Zhi Xiang Lin <zhixiang94@gmail.com>
2015-04-12 01:35:01 -0400

    added dashboard

--- compile.sh
+++ compile.sh
@@ -5,3 +5,4 @@ compile="$(which clientjade) -c"
 $compile ./templates/index.jade > ./packages/system/public/assets/jade/index.js
 $compile ./templates/lender.jade > ./packages/system/public/assets/jade/lender.js
 $compile ./templates/borrower.jade > ./packages/system/public/assets/jade/borrower.js
+$compile ./templates/dashboard.jade > ./packages/system/public/assets/jade/dashboard.js
