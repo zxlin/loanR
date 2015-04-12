e45a0a89a3c110d9946d283da857d880bcac5610
fpayer <fpayer@tacnetsol.com>
2015-04-11 21:32:53 -0400

    Css fix

--- packages/socket/auth.js
+++ packages/socket/auth.js
@@ -6,7 +6,9 @@ var app;
 
 var expected_time = function(amount, interest, monthly) {
   for (var t = 1; t <= 10000; t++) {
-    if (Math.pow(amount * (1 + interest), t) - t * monthly <= 0) {
+    amount -= monthly;
+    amount *= (1 + interest);
+    if (amount <= 0) {
       return t;
     }
   }
